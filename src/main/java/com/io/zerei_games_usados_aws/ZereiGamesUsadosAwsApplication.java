b81a438905351abd05209f5c75a8c9012dfeaefd
joaokage1 <j_vgomes@hotmail.com>
2024-08-18 17:17:49 -0300

    Initial commit

---
+++ src/main/java/com/io/zerei_games_usados_aws/ZereiGamesUsadosAwsApplication.java
@@ -0,0 +1,13 @@
+package com.io.zerei_games_usados_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZereiGamesUsadosAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZereiGamesUsadosAwsApplication.class, args);
+	}
+
+}
